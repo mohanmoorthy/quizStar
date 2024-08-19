6721b4304d636a7800a720fc31d849b66a05f531
Mohanmoorthy <mohan@Mohans-Macbook.local>
2024-08-19 15:31:03 +0530

    first commit

---
+++ quizStar/quizStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizStarApp.swift
+//  quizStar
+//
+//  Created by Mohanmoorthy on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct quizStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
